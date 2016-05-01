fb5b13c3edc0026df3371b620339bd2e6d711cbe
Adam Jaso <mkltwain@gmail.com>
2016-04-30 20:47:09 -0700

    initial commit

---
+++ gpsd.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const GpsdClient = require('./lib/GpsdClient');
+const SandGrain = require('sand-grain');
+
+class Gpsd extends SandGrain {
+
+  constructor() {
+    super();
+    this.name = 'gpsd';
+    this.configName = 'gpsd';
+    this.defaultConfig = require('./defaultConfig');
+    this.version = require('./package').version;
+  }
+
+  bindToContext(ctx) {
+    let clients = [];
+    ctx.gpsd = new GpsdClient(this.config);
+    ctx.gpsd.on('client', client => {
+      clients.push(client);
+    });
+    ctx.on('end', () => {
+      for (let client of clients) {
+        if (client.isConnected()) {
+          this.log('disconnecting client...');
+          client.disconnect();
+        }
+      }
+    });
+  }
+
+}
+
+module.exports = Gpsd;
